68383a01073afa0435ed958672b5166ab2181181
TronicDK <casper@famkristiansen.dk>
2020-01-12 15:50:56 +0100

    Update 1

---
+++ www/community/card-thommaslov/lovelace-slider-entity-row-7/README.md
@@ -0,0 +1,100 @@
+slider-entity-row
+=================
+
+Add a slider to rows in lovelace [entities](https://www.home-assistant.io/lovelace/entities/) cards.
+
+For installation instructions [see this guide](https://github.com/thomasloven/hass-config/wiki/Lovelace-Plugins).
+
+Install `slider-entity-row.js` as a `module`.
+
+## Usage
+Add this to an entities card:
+
+```yaml
+type: entities
+entities:
+  - light.bed_light
+  - type: custom:slider-entity-row
+    entity: light.kitchen_lights
+```
+
+![slider-entity-row](https://user-images.githubusercontent.com/1299821/59467898-15b16600-8e31-11e9-9924-53b108572d3a.png)
+
+Currenly supported entity domains:
+
+- `light` - set brightness
+- `media_player` - set volume
+- `climate` - set temperature
+- `cover` - set position
+- `fan` - set speed (assumes first setting is `off`)
+- `input_number` - set value (only if `mode: slider`)
+- `input_select` - select option
+
+![domains](https://user-images.githubusercontent.com/1299821/59467899-1813c000-8e31-11e9-8abd-34c887a7db2a.png)
+
+### Options
+
+- `toggle: true` - Show a toggle instead of current state
+- `hide_state: true` - Do not display current state
+- `hide_when_off: true` - Hide the slider when state is `off`
+- `full_row: true` - Hide icon and name and stretch slider to full width
+- `min: <value>` - Set minimum value of slider
+- `max: <value>` - Set maximum value of slider
+- `step: <value>` - Set step size of slider
+- `attribute: <value>` - Select which attribute the slider should control
+
+```yaml
+type: entities
+title: Options
+entities:
+  - type: custom:slider-entity-row
+    entity: light.bed_light
+    name: Default
+  - type: custom:slider-entity-row
+    entity: light.bed_light
+    name: toggle
+    toggle: true
+  - type: custom:slider-entity-row
+    entity: light.bed_light
+    name: hide_state
+    hide_state: true
+  - type: custom:slider-entity-row
+    entity: light.ceiling_lights
+    name: hide_when_off
+    hide_when_off: true
+  - type: custom:slider-entity-row
+    entity: light.ceiling_lights
+    name: hide_when_off + toggle
+    hide_when_off: true
+    toggle: true
+  - type: section
+    label: full_row
+  - type: custom:slider-entity-row
+    entity: light.bed_light
+    name: hide_state
+    full_row: true
+```
+
+![options](https://user-images.githubusercontent.com/1299821/59467902-19dd8380-8e31-11e9-9173-97c9b6be3179.png)
+
+#### Attributes
+Currently, the following attribute settings are supported.
+
+**For `light` domain:**
+
+- `brightness` - default
+- `color_temp`
+- `hue`
+- `saturation`
+- `red`
+- `green`
+- `blue`
+- `effect`
+
+**For `cover` domain:**
+
+- `position` - default
+- `tilt`
+
+---
+<a href="https://www.buymeacoffee.com/uqD6KHCdJ" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/white_img.png" alt="Buy Me A Coffee" style="height: auto !important;width: auto !important;" ></a>
---
+++ www/community/card-thommaslov/lovelace-slider-entity-row-7/src/light-controller.js
@@ -0,0 +1,164 @@
+import {Controller} from "./controller.js";
+
+export class LightController extends Controller {
+
+  get attribute() {
+    return this._config.attribute || "brightness";
+  }
+
+  get _value() {
+    if (this.stateObj.state !== "on") return 0;
+    switch (this.attribute) {
+      case "color_temp":
+        return Math.ceil(this.stateObj.attributes.color_temp);
+      case "brightness":
+        return Math.ceil(this.stateObj.attributes.brightness*100.0/255)
+      case "red":
+        return this.stateObj.attributes.rgb_color ? Math.ceil(this.stateObj.attributes.rgb_color[0]) : 0;
+      case "green":
+        return this.stateObj.attributes.rgb_color ? Math.ceil(this.stateObj.attributes.rgb_color[1]) : 0;
+      case "blue":
+        return this.stateObj.attributes.rgb_color ? Math.ceil(this.stateObj.attributes.rgb_color[2]) : 0;
+      case "hue":
+        return this.stateObj.attributes.hs_color ? Math.ceil(this.stateObj.attributes.hs_color[0]): 0;
+      case "saturation":
+        return this.stateObj.attributes.hs_color ? Math.ceil(this.stateObj.attributes.hs_color[1]): 0;
+      case "effect":
+        if(this.stateObj.attributes.effect_list)
+          return (this.stateObj.attributes.effect_list.indexOf(this.stateObj.attributes.effect));
+        return 0;
+      default:
+        return 0;
+    }
+  }
+
+  get _step() {
+    switch (this.attribute) {
+      case "effect":
+        return 1;
+      default:
+        return 5;
+    }
+  }
+
+  get _min() {
+    switch (this.attribute) {
+      case "color_temp":
+        return this.stateObj.attributes.min_mireds;
+      default:
+        return 0;
+    }
+  }
+  get _max() {
+    switch (this.attribute) {
+      case "color_temp":
+        return this.stateObj.attributes.max_mireds;
+      case "red":
+      case "green":
+      case "blue":
+        return 255;
+      case "hue":
+        return 360;
+      case "effect":
+        return this.stateObj.attributes.effect_list ? this.stateObj.attributes.effect_list.length - 1 : 0;
+      default:
+        return 100;
+    }
+  }
+
+  set _value(value) {
+    let attr = this.attribute;
+    let on = true;
+    let _value;
+    switch (attr) {
+      case "brightness":
+        value = Math.ceil(value/100.0*255);
+        if (!value) on = false;
+        break;
+      case "red":
+      case "green":
+      case "blue":
+        _value = this.stateObj.attributes.rgb_color || [0,0,0];
+        if (attr === "red") _value[0] = value;
+        if (attr === "green") _value[1] = value;
+        if (attr === "blue") _value[2] = value;
+        value = _value;
+        attr = "rgb_color";
+        break;
+      case "hue":
+      case "saturation":
+        _value = this.stateObj.attributes.hs_color || [0,0];
+        if (attr === "hue") _value[0] = value;
+        if (attr === "saturation") _value[1] = value;
+        value = _value;
+        attr = "hs_color";
+        break;
+      case "effect":
+        value = this.stateObj.attributes.effect_list[value];
+        attr = "effect";
+        break;
+    }
+
+    if (on) {
+      this._hass.callService("light", "turn_on", {
+        entity_id: this.stateObj.entity_id,
+        [attr]: value,
+      });
+    } else {
+      this._hass.callService("light", "turn_off", {
+        entity_id: this.stateObj.entity_id,
+      });
+    }
+  }
+
+  get string() {
+    if (this.stateObj.state === "off")
+      return this._hass.localize("state.default.off");
+    switch (this.attribute) {
+      case "color_temp":
+        return `${this.value} K`;
+      case "brightness":
+      case "saturation":
+        return `${this.value} %`;
+      case "hue":
+        return `${this.value} °`;
+      case "effect":
+        return this.stateObj.attributes.effect;
+      default:
+        return this.value;
+    }
+  }
+
+  get hasSlider() {
+    switch (this.attribute) {
+      case "brightness":
+        if ("brightness" in this.stateObj.attributes) return true;
+        if (("supported_features" in this.stateObj.attributes) &&
+          (this.stateObj.attributes.supported_features & 1)) return true;
+        return false;
+      case "color_temp":
+        if ("color_temp" in this.stateObj.attributes) return true;
+        if (("supported_features" in this.stateObj.attributes) &&
+          (this.stateObj.attributes.supported_features & 2)) return true;
+        return false;
+      case "red":
+      case "green":
+      case "blue":
+        if ("rgb_color" in this.stateObj.attributes) return true;
+        if (("supported_features" in this.stateObj.attributes) &&
+          (this.stateObj.attributes.supported_features & 16)) return true;
+        return false;
+      case "hue":
+      case "saturation":
+        if ("hs_color" in this.stateObj.attributes) return true;
+        if (("supported_features" in this.stateObj.attributes) &&
+          (this.stateObj.attributes.supported_features & 16)) return true;
+        return false;
+      case "effect":
+        if ("effect" in this.stateObj.attributes) return true;
+        return false;
+      default:
+        return false;
+    }
+  }
+}
